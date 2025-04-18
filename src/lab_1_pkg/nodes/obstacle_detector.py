df2d4805b519dab29d898e6d540fd6918b49b7b4
JOSE QUIMI <jquimi@uc.cl>
2025-04-18 16:55:39 -0400

    PArametros y avance deteccion del nodo obstacle detector

--- src/lab_1_pkg/nodes/obstacle_detector.py
+++ src/lab_1_pkg/nodes/obstacle_detector.py
@@ -26,4 +26,15 @@ class ObstacleDetector(Node):
         self.current_cv_depth_image = None
 
     def depth_image_callback(self, msg):
-        pass
+        try:
+            self.current_cv_depth_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough') # Para mantener el tipo de datos, luego saber si vienen en milimetros o metros, esto retorna una matriz, si es en profundidad, dara una matriz 2D con la profundidad en la codificacion del sensor, donde cada elemento de la matriz es un pixel de la imagen#
+
+            if msg.encoding == '16UC1':
+                self.current_cv_depth_image = self.current_cv_depth_image.astype(np.float32) / 1000.0  # Convertir a metros, en caso de que venga en milimetros
+
+            #!TODO Separar la imagen y medir la distancia entre el robot y el objeto
+
+        except Exception as e:
+            self.get_logger().error(f'Error al obtener profundidad u obtener los datos: {e}')
+
+
