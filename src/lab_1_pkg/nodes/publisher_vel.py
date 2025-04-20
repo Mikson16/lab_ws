81099952918468f497207707f79fb6b9357c8eae
Maximiliano Pisfil <maxi.pi@uc.cl>
2025-04-20 15:26:09 -0400

    Actualizado publisher: Connie (funciona bien). Corregir problema simulacion

--- src/lab_1_pkg/nodes/publisher_vel.py
+++ src/lab_1_pkg/nodes/publisher_vel.py
@@ -1,64 +1,88 @@
 #!/usr/bin/env python3
 
-from std_msgs.msg import String
 import rclpy
 from rclpy.node import Node
-import threading
+from std_msgs.msg import String
+#Agregue Twist para poder enviar velocidades al robot
+from geometry_msgs.msg import Twist
+#Agregue los eventos de parachoques para detectar colisiones
+from kobuki_ros_interfaces.msg import BumperEvent
+# sudo apt install ros-humble-kobuki-ros-interfaces
+
 
 class NodoPublicador(Node):
-
     def __init__(self):
         super().__init__("nodo_publicador")
+
         # Nos suscribimos al topico para leer la tecla y la informacion del parachoque
         self.subscriber_key = self.create_subscription(String, 'tecla_presionada', self.recibir_tecla, 10)
-        
-        # Variable que se actualiza segun si hay o no un obstaculo
-        self.obstaculo = False
-       
-        self.movimiento = None
+        # Suscripción para detectar choques frontales
+        self.create_subscription(BumperEvent, '/events/bumper', self.recibir_choque, 10)
+        # Publicador para enviar mensajes de velocidad en formato Twist
+        self.publisher_vel = self.create_publisher(Twist, '/commands/velocity', 10)
+        # Variables de estado
+        self.obstaculo = False 
+        self.movimiento_actual = Twist() 
+        # Timer para enviar_velocidad periódicamente (Elimine el metodo aplicar_velocidad(self))
+        self.timer = self.create_timer(0.1, self.enviar_velocidad)
 
-    def recibir_tecla(self, data): # Callback
-        # Accedemos al atributo de la EDD
-        tecla = data.data
-        self.get_logger().info(f"La tecla recibida fue: {tecla}")
-        self.manejador_eventos(tecla)
-        
-    
-    def recibir_choque(self, data):
-        # Metodo que recibe la informacion del choque y actualiza self.obstaculo
-        pass
+        self.get_logger().info("Nodo que realiza el movimiento inicializado")
 
-    def manejador_eventos(self, tecla):
-        """
-        Este metodo es activado al recibir una tecla 
-        """
-        if self.obstaculo == False:
-            # Traducimos la tecla en la estructura valida
-            if tecla == "w":
-                self.movimiento = "Adelante"
-            elif tecla == "s":
-                self.movimiento = "atrash"
-            # Creamos un thread para el envio de velocidad
-            self.aplicar_velocidad()
+    def recibir_tecla(self, msg):
+        # Pasar todos los mensajes a minuscula xsi las moscas
+        tecla = msg.data.lower()
 
-    def aplicar_velocidad(self):
-        """
-        Este metodo crea el timer para enviar la velocidad
-        """
-        # Metodos 
-        tiempo_periodo = 0.1 #segundos
-        self.timer = self.create_timer(tiempo_periodo, self.enviar_velocidad)
-        
+        # Condición de que si existe un obstaculo no se pueda mover
+        if self.obstaculo:
+            self.get_logger().info("Aiuda, no me puedo mover, he chocao chaval :c")
+            return
+
+        # Eliminamos el mensaje anterior para evitar que tenga valores viejos
+        twist = Twist()
+
+        # Condicionales para saber que tecla es la recibida
+        if tecla == "i":
+            twist.linear.x = 0.2  # Adelane
+        elif tecla == "j":
+            twist.linear.x = -0.2  # Atrashhh
+        elif tecla == "a":
+            twist.angular.z = 1.0  # Giro izq
+        elif tecla == "s":
+            twist.angular.z = -1.0  # Giro der
+        elif tecla == "q":
+            twist.linear.x = 0.2
+            twist.angular.z = 1.0  # Avanza y gira izq
+        elif tecla == "w":
+            twist.linear.x = 0.2
+            twist.angular.z = -1.0  # Avanza y gira der
+        else:
+            self.get_logger().info(f"Tecla no valida: {tecla}")
+            return
+        # Definimos el movimiento actual pa despues enviarlo
+        self.movimiento_actual = twist 
+
+    def recibir_choque(self, msg):
+        # Ay choco el robot bobo (se presiono el bumper frontal)
+        if msg.state == 1:
+            self.get_logger().info("El bobo de Julian Alberto ha chocado")
+            self.obstaculo = True
+            # Reiniciamosel Twits
+            self.movimiento_actual = Twist()
+        # No hay obstaculos (el robot ya no está chocado)
+        elif msg.state == 0:
+            self.get_logger().info("Julian Alberto puede moverse libremente otra vez")
+            self.obstaculo = False
 
     def enviar_velocidad(self):
-        self.get_logger().info(f'Estamos enviando: {self.movimiento}')
-        
+        self.get_logger().info(f'Estamos enviando: {self.movimiento_actual}')
+        # Publicamo el movimiento B)
+        self.publisher_vel.publish(self.movimiento_actual)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = NodoPublicador()
-    rclpy.spin(node)
-    node.destroy_node()
+    nodo = NodoPublicador()
+    rclpy.spin(nodo)
+    nodo.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
