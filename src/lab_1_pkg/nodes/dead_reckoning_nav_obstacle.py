39eed0eaf2e77d71d4f6bbf703779cd8f7ecd2db
Maximiliano Pisfil <maxi.pi@uc.cl>
2025-04-24 03:46:14 -0400

    Corregido la separacion de los hilos

--- src/lab_1_pkg/nodes/dead_reckoning_nav_obstacle.py
+++ src/lab_1_pkg/nodes/dead_reckoning_nav_obstacle.py
@@ -12,42 +12,46 @@ import math
 import time
 from parametros import VELOCIDAD_LINEAL, VELOCIDAD_ANGULAR
 import threading
+import queue
+
+
 
 class DeadReckoningNav(Node):
 
     def __init__(self):
-        super().__init__('dead_reckoning_nav')
+        super().__init__('dead_reckoning_nav_obstacles')
         #Suscripción a Pose_Loader
-        self.subscription_array = self.create_subscription(PoseArray,'goal_list',self.accion_mover,10)
+        self.subscription_array = self.create_subscription(PoseArray,'goal_list',self.recibir_posiciones,10)
+        # Cola de las posiciones FIFO
+        self.cola_posiciones = queue.Queue()
+        # Creamos un thread que sera el responsable de revisar las posiciones y ejecutar las velocidades designadsas
+        self.hilo = threading.Thread(target=self.accion_mover)
+
+        # Bandera para indicar si hay un obstaculo o no
+        self.bandera_obstaculo = False
+        
         #Publicador de velocidades
         self.publisher_cmd_vel = self.create_publisher(Twist,'/cmd_vel_mux/input/navigation',10)
         # Variable para guardar la pose anterior
         self.ultima_pose = None
-        self.get_logger().info('Nodo dead_reckoning_nav iniciado.')
+        self.get_logger().info('Nodo dead_reckoning_nav_obstacle iniciado.')
 
         # Visualizacion de obstaculos por medio del mensaje publicado por obstacle detector
         self.subscription_obstacle = self.create_subscription(Vector3,'/ocupancy_state',self.accion_obstaculo,10)
         self.get_logger().info('Suscrito al topico de obstaculos.')
-    
-    def accion_obstaculo(self, msg):
-        """
-        Input: msg del topico /ocupancy_state
-        Output: imprimir el estado de los obstaculos de momento
-        """
+        
+        #! Esto de abajho esta bien
+        # Ejecutamos el hilo
+        self.hilo.start()
 
-        self.get_logger().info(f"\nRecibido el mensaje de ocupacion: {msg}")
 
-        if msg.x > 0.0 or msg.y > 0.0 or msg.z > 0.0:
-            self.get_logger().info(f"Estado de los obstaculos: Izquierda: {msg.x}, Centro: {msg.y}, Derecha: {msg.z}")
-
-    def accion_mover(self, msg):
+    def recibir_posiciones(self, msg):
         """
-        Input: msg del topico goal_list recibe una lista de posiciones
-        Output: llamar al metodo para actualizar la posicion
+        Este método recibe la lista de posiciones que envia pose_loader
+        y las agrega a una queue FIFO
         """
-        #Recibe la lista de poses objetivo
-        self.get_logger().info(f'Recibidas {len(msg.poses)} poses.')
-
+        self.get_logger().info(f"Recibidas las posiciones")
+        
         for i, pose in enumerate(msg.poses):
             x = pose.position.x
             y = pose.position.y
@@ -55,12 +59,39 @@ class DeadReckoningNav(Node):
             _, _, theta = euler_from_quaternion((q.x, q.y, q.z, q.w))
 
             goal = [x, y, theta]
-            self.get_logger().info(f" ")
+            self.cola_posiciones.put(goal)
+        
+        
 
-            # self.get_logger().info(f"Moviendo al objetivo {i}: {goal}")
-            self.mover_robot_a_destino(goal)
+    def accion_mover(self):
+        """
+        Metodo ejecutado en el hilo paralelo al main. 
+        Se encarga de revisar si hay posiciones a las cuales llegar 
+        Llama a mover_robot_a_destino para 
+        """
+        while rclpy.ok():
+            #self.get_logger().info(f"Estoy en el hilo paralelo")
+            # self.get_logger().info(f"Estado bandera {self.bandera_obstaculo}")
+            
+            if not self.cola_posiciones.empty():
+                goal = self.cola_posiciones.get()
+                # Pasamos la posicion para que sea convertida en velocidad
+                self.mover_robot_a_destino(goal)
 
-        self.get_logger().info("Todos los objetivos fueron alcanzados.")
+    def accion_obstaculo(self, msg):
+        """
+        Input: msg del topico /ocupancy_state
+        Output: Cambiar el estado de la bander e imprimir 
+        """
+
+        #self.get_logger().info(f"\nRecibido el mensaje de ocupacion: {msg}")
+        if msg.x > 0.0 or msg.y > 0.0 or msg.z > 0.0:
+            #self.get_logger().info(f"Estado de los obstaculos: Izquierda: {msg.x}, Centro: {msg.y}, Derecha: {msg.z}")
+            #self.get_logger().info(f"Obstaculos: Izquierda: {msg.x}, Centro: {msg.y}, Derecha: {msg.z}")
+            self.bandera_obstaculo = True
+        elif msg.x == 0.0 and msg.y == 0.0 and msg.z == 0.0:
+            self.bandera_obstaculo = False
+
 
     def mover_robot_a_destino(self, goal):
         """
@@ -99,6 +130,7 @@ class DeadReckoningNav(Node):
         # self.get_logger().info(f"Velocidad enviada con el tiempo {msg_to_send}")
         self.aplicar_velocidad(msg_to_send)
 
+
     def aplicar_velocidad(self, msg_to_send):
         """
         input: [v, 0, t] o [0,w,t] 
@@ -117,12 +149,33 @@ class DeadReckoningNav(Node):
         twist.angular.y = 0.0
         twist.angular.z = float(w_angular)
 
-        # Comenzamos a contar el tiempo transcurrido
-        tiempo_inicio = time.time()
-        while (time.time() - tiempo_inicio) < tiempo:
-            # Publicar el mensaje de velocidad
-            self.publisher_cmd_vel.publish(twist)
+        
+        bandera_seguir = True
+        ultimo_estado = False
+        twist_blanco = Twist()
+        while bandera_seguir:
+            self.get_logger().info(f"Estado bandera {self.bandera_obstaculo}")
 
+            if self.bandera_obstaculo == False:
+                # Si no hay obstaculos, podemos enviar la velocidad
+                self.publisher_cmd_vel.publish(twist)
+                if ultimo_estado == False:
+                    # Marcamos el tiempo de inicio
+                    tiempo_inicio = time.time()
+                    ultimo_estado = True
+                if time.time() - tiempo_inicio > tiempo:
+                    bandera_seguir = False
+            else:
+                # En el caso en que existan obstaculos, enviamos velocidad de 0
+                self.publisher_cmd_vel.publish(twist_blanco)
+                if ultimo_estado == True:
+                    # Esto solo lo ejecutamos cuando pasa de no haber un obst. a haber un obst.
+                    ultimo_estado = False
+                    tiempo_transcurrido = time.time() - tiempo_inicio
+                    tiempo -= tiempo_transcurrido        
+        
+        
+        
 
 def main(args=None):
     rclpy.init(args=args)
